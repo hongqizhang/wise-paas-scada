36488c02bfb0589ed4f7894c5599ab7d46c4a6ea
Steven.Li <StevenHY.Li@advantech.com.tw>
2017-07-10 14:58:29 +0800

    [modify] add mongodb connection options

--- db/mongodb.js
+++ db/mongodb.js
@@ -10,8 +10,19 @@ function _connect (conf) {
     return;
   }
 
+  let options = {
+    db: { native_parser: true },
+    server: {
+      auto_reconnect: true,
+      poolSize: 5,
+      socketOptions: {
+        connectTimeoutMS: 30000,
+        socketTimeoutMS: 30000
+      }
+    }
+  };
   mongoose.connect(util.format('mongodb://%s:%s@%s:%d/%s',
-    conf.username, conf.password, conf.hostname, conf.port, conf.database));
+    conf.username, conf.password, conf.hostname, conf.port, conf.database), options);
 
   mongoose.Promise = Promise;
   let db = mongoose.connection;
