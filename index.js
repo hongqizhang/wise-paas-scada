11f16bb0144d9ae8e73390f57cbc54494ec4c483
Steven.Li <StevenHY.Li@advantech.com.tw>
2017-07-21 16:01:20 +0800

    [add] support mqtt

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 const datastore = require('./api/datastore.js');
 const deviceManager = require('./api/device-manager.js');
 const waamqp = require('./communication/waamqp.js');
+const wamqtt = require('./communication/wamqtt.js');
 const waTopics = require('./common/watopics.js');
 
 module.exports.datastore = datastore;
 module.exports.deviceManager = deviceManager;
 module.exports.waamqp = waamqp;
+module.exports.wamqtt = wamqtt;
 module.exports.amqpTopics = waTopics.amqpTopics;
 module.exports.mqttTopics = waTopics.mqttTopics;
