945addeed44abcab5ff1f4b06962d2f64bef894e
Steven.Li <StevenHY.Li@advantech.com.tw>
2017-12-01 15:56:44 +0800

    [modify] combine data and config queue

--- communication/waamqp.js
+++ communication/waamqp.js
@@ -33,15 +33,7 @@ function _ack (msg, all) {
   }
 }
 
-function _connect (uri, type, callback) {
-  /* let url = {
-    protocol: 'amqp',
-    hostname: conf.hostname || '127.0.0.1',
-    port: conf.port || 5672,
-    username: conf.username,
-    password: conf.username
-  }; */
-
+function _connect (uri, callback) {
   amqp.connect(uri, (err, conn) => {
     if (err) {
       console.error('[AMQPConnectError] ' + err);
@@ -52,57 +44,49 @@ function _connect (uri, type, callback) {
     });
     conn.on('close', () => {
       console.error('[AMQP] Connection close ! Reconnecting....');
-      setTimeout(_connect(uri, type, callback), 1000);
+      setTimeout(() => { _connect(uri, callback); }, 1000);
     });
     connection = conn;
     connection.createChannel((err, ch) => {
       if (err) {
-        callback(err);
-        return;
+        return callback(err);
       }
-      // options.type
-      switch (type) {
-        case 'config':
-          ch.assertQueue(amqpQueue.cfgQ, { durable: true });
-          // ch.assertQueue(amqpQueue.notifyQ, { durable: true });
-          ch.unbindQueue('waCfgQ', exchangeName, '.wisepaas.*.scada.*.cfg');
-          ch.deleteQueue('waCfgQ');
-          // binding mqtt topic to queue, and '/' must be replaced to '.' in topic
-          ch.bindQueue(amqpQueue.cfgQ, exchangeName, amqpTopics.configTopic.replace(/\//g, '.'));
-          // ch.bindQueue(amqpQueue.notifyQ, exchangeName, amqpTopics.notifyTopic.replace(/\//g, '.'));
-          ch.prefetch(1);
-          ch.consume(amqpQueue.cfgQ, (msg) => {
-            let buff = msg.fields.routingKey.split('.');
-            if (buff.length !== 5) {
-              return;
-            }
-            // msg.tenantId = buff[2];
-            msg.scadaId = buff[3];
-            events.emit('config', msg);
-          }, { noAck: false });
-          break;
-        case 'data':
-          ch.assertQueue(amqpQueue.dataQ, { durable: true });
 
-          ch.unbindQueue('waDataQ', exchangeName, '.wisepaas.*.scada.*.data');
-          ch.deleteQueue('waDataQ');
-          // binding mqtt topic to queue, and '/' must be replaced to '.' in topic
-          ch.bindQueue(amqpQueue.dataQ, exchangeName, amqpTopics.dataTopic.replace(/\//g, '.'));
+      ch.unbindQueue('waCfgQ', exchangeName, '.wisepaas.*.scada.*.cfg');
+      ch.deleteQueue('waCfgQ');
+      ch.unbindQueue('waDataQ', exchangeName, '.wisepaas.*.scada.*.data');
+      ch.deleteQueue('waDataQ');
 
-          ch.prefetch(1);
-          ch.consume(amqpQueue.dataQ, (msg) => {
-            let buff = msg.fields.routingKey.split('.');
-            if (buff.length !== 5) {
-              return;
-            }
-            // msg.tenantId = buff[2];
-            msg.scadaId = buff[3];
-            events.emit('data', msg);
-          }, { noAck: false });
-          break;
-        default:
-          break;
-      }
+      ch.assertQueue(amqpQueue.cfgQ, { durable: true });
+      ch.assertQueue(amqpQueue.dataQ, { durable: true });
+      // ch.assertQueue(amqpQueue.notifyQ, { durable: true });
+
+      // binding mqtt topic to queue, and '/' must be replaced to '.' in topic
+      ch.bindQueue(amqpQueue.cfgQ, exchangeName, amqpTopics.configTopic.replace(/\//g, '.'));
+      ch.bindQueue(amqpQueue.dataQ, exchangeName, amqpTopics.dataTopic.replace(/\//g, '.'));
+      // ch.bindQueue(amqpQueue.notifyQ, exchangeName, amqpTopics.notifyTopic.replace(/\//g, '.'));
+
+      ch.prefetch(1);
+
+      ch.consume(amqpQueue.cfgQ, (msg) => {
+        let buff = msg.fields.routingKey.split('.');
+        if (buff.length !== 5) {
+          return;
+        }
+        // msg.tenantId = buff[2];
+        msg.scadaId = buff[3];
+        events.emit('config', msg);
+      }, { noAck: false });
+
+      ch.consume(amqpQueue.dataQ, (msg) => {
+        let buff = msg.fields.routingKey.split('.');
+        if (buff.length !== 5) {
+          return;
+        }
+        // msg.tenantId = buff[2];
+        msg.scadaId = buff[3];
+        events.emit('data', msg);
+      }, { noAck: false });
 
       channel = ch;
       callback(null);
