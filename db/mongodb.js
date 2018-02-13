7dc41690f4bc15b6a1ebf8d3dbd8953659ea8b03
Steven.Li <StevenHY.Li@advantech.com.tw>
2018-02-13 17:55:04 +0800

    [modify] remove garbage code

--- db/mongodb.js
+++ db/mongodb.js
@@ -33,30 +33,17 @@ function _connect (conf) {
     if (err && err.message) {
       console.error(err.message);
     }
-    // _disconnect();
   });
   db.on('connected', () => {
     console.log('[mongodb] Connect success !');
   });
   db.on('disconnected', () => {
     console.log('[mongodb] Disconnected !');
-    /* setTimeout(() => {
-      _reconnect(conf, options);
-    }, 3000); */
   });
 }
 
 function _disconnect () {
   mongoose.disconnect();
-}
-
-function _reconnect (conf, options) {
-  console.log('[mongodb] Disconnected ! Try to connect...');
-  mongoose.connect(util.format('mongodb://%s:%s@%s:%d/%s',
-    conf.username, conf.password, conf.host, conf.port, conf.database), options)
-  .catch((err) => {
-    console.error(err.message);
-  });
 }
 
 function _isConnected () {
